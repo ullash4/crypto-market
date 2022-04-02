17c4dff5691f40d435ab4b26684de7490c6871bc
ullash4 <programmerullash@gmail.com>
2022-04-02 23:58:04 +0600

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-5xl font-semibold'>Welcome to Crypto Market</h1>
+            <button className='m-5 px-5 py-2 text-xl font-semibold bg-indigo-400 rounded-xl' >Explore Crypto</button>
+        </div>
+    );
+};
+
+export default Home;
